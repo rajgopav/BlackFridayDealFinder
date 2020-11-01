d049263628c64ee6fc0c916c7e9aaf0bf46a4337
adithyas98 <adi.shastry@gmail.com>
2020-11-01 11:07:55 -0600

    Created fields to add the store and the link of the offer

--- product.py
+++ product.py
@@ -7,12 +7,16 @@ class Product:
     description=''#this will hold the price of the Product
     pricePrev=0.0#Holds the previous pricePrev
     priceCurr=0.0#holds the current price
+    link=""#this will hold the link for the Product
+    store=""#this will hold the store
 
-    def __init__(self,name,description,pricePrev,priceCurr):
+    def __init__(self,name,description,pricePrev,priceCurr,link,store):
         self.description=description
         self.name=name
         self.pricePrev=pricePrev
         self.priceCurr=priceCurr
+        self.link=link
+        self.store=store
 
     def getDescription(self):
         return self.description
@@ -25,3 +29,9 @@ class Product:
         return (self.getPriceDrop)/self.pricePrev *100
     def getName(self):
         return self.name
+
+    def getStore(self):
+        #this will get the store
+        return self.store
+    def getLink(self):
+        return self.store
