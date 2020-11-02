4efc16b744b7e6616f227600c21e1649da981977
adithyas98 <adi.shastry@gmail.com>
2020-11-02 15:27:15 -0600

    Got the Product urls for Amazon

--- dealFinder.py
+++ dealFinder.py
@@ -3,7 +3,7 @@ This will be the main file that will serve as a way to search through all the
 websites based on the search input that the user gives.
 '''
 from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen as uReq
+import urllib.request as uReq
 from product import Product
 import pandas as pd
 
@@ -17,10 +17,21 @@ class DealFinder:
     product=[]#this will hold a list of the products found through the websites
     numDeals=0#This will hold the number of top deals that user wants to see
     topDeals=[]#this will hold the top deals
+    depth=[]#the number of pages deep the script should go
 
     def __init__(self,searchList,numDeals=15):
         self.search=searchList
         self.numDeals=numDeals#Will default to the top 15 deals
+
+    #A method that will open pages with bs4
+    def saveThePage(self,url):
+        hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
+        req = uReq.Request(url, headers=hdr)
+        response = uReq.urlopen(req)
+        content= response.read()
+        response.close()
+        return soup(content,'html.parser')
+
 
 
 
@@ -40,8 +51,7 @@ class DealFinder:
     Now we will present the functions that will find the products from websites
     '''
     #This will find the deals on amazon
-    def findonAmazon(self,itemName):
-        "Base Url:https://www.amazon.com/s?k=nintendo+switch+games&page=2&qid=1604344724&ref=sr_pg_2"
+    def findonAmazon(self,itemName,depth=11):
         item=''
         words=itemName.split()
         for i in range(0,len(words)):
@@ -51,8 +61,24 @@ class DealFinder:
             else:
                 #we are at the end
                 item = item + "+{}".format(words[i])
+
+        products=[]#will hold the links to the product pages
+        for i in range(1,depth+1):
+            url="https://www.amazon.com/s?k={}&page={}&qid=1604344724&ref=sr_pg_{}".format(item,i,i)
+            #with this url we can go through and get all the products on the page
+            result=self.saveThePage(url)#this will create a soup object we can interface with
+            for x in result.find_all("h2",attrs={'class':'a-size-mini a-spacing-none a-color-base s-line-clamp-2'}):
+                products.append(x.a['href'])
         
-        baseUrl="https://www.amazon.com/s?k={}&page={}&qid=1604344724&ref=sr_pg_2".format(item,1)
+
+
+
+
+
+        #products.append(self.saveThePage(pUrl))
+
+
+
 
 
 
@@ -72,4 +98,4 @@ class DealFinder:
 if __name__ == '__main__':
     list=['Nintendo Switch']
     s=DealFinder(list,10)
-    s.findonAmazon(list[0])
+    s.findonAmazon(list[0],2)
